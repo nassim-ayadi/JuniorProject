b3f83f7c2fa8c23c515d73960037333431df461d
Nassim Ayadi <nassim.ayadi@ADDIXO.LOCAL>
2023-11-15 20:35:23 +0100

    Create and style the home page

--- src/components/LoginSignUp.js
+++ src/components/LoginSignUp.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import "./LoginSignUp.css"
 
-import logo from "C:/Users/feres/OneDrive/Desktop/JuniorProject/src/smulogo.png"
+// import logo from "C:/Users/feres/OneDrive/Desktop/JuniorProject/src/smulogo.png"
+import logo from '../smulogo.png';
 
 import user_icon from "./Assets/person.png"
 import email_icon from "./Assets/password.png"
